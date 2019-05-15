60af963d8012f3b7273276fdc6f5f3cd69be8514
Derek Townsend <derekjavon2@gmail.com>
2019-05-15 16:22:07 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    sentences=self.split(%r{[.?!]})
+    sentences.delete("")
+    sentences.count
   end
 end
+
+p "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
