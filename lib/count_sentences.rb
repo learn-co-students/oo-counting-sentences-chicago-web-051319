e6d8f96704bd7b52363415fa662f513e4963e5b9
Jooyon Won <jyon1204@gmail.com>
2019-05-16 10:25:40 -0500

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # self.split(/[.]/).reject {|x| x.empty?}.size
+    count = 0
+    self.split.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        count += 1
+      end
+    end
+    count
   end
 end
+
+# complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# arr = ["This, well, is a sentence.",  "This is too!! And so is this, I think? Woo..."]
